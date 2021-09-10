4f9b9b075375bd68093c6cc4e0952c53a0c8fbfc
Paulo <Paulo@192.168.1.11>
2021-09-10 14:23:48 -0300

    implementacion findall en service

--- digital-web/src/main/java/com/digital/service/impl/CategoriasServiceImpl.java
+++ digital-web/src/main/java/com/digital/service/impl/CategoriasServiceImpl.java
@@ -1,13 +1,27 @@
 package com.digital.service.impl;
 
+import java.util.List;
+
 import com.digital.dao.impl.CategoriasDaoImpl;
 import com.digital.domain.Categorias;
+import com.digital.exception.GenericException;
 import com.digital.service.CategoriasService;
+import com.digital.service.ServiceException;
 
 public class CategoriasServiceImpl extends AbstractBaseService<Categorias> implements CategoriasService{
 
 	public CategoriasServiceImpl() {
 		super(new CategoriasDaoImpl());
 	}
+	
+	@Override
+	public List<Categorias> findAll() throws ServiceException {
+		try {
+			return this.dao.findAll();
+		} catch (GenericException e) {
+			throw new ServiceException("no se pudo obtener la lista mi rey", e.getCause());
+		}
+	}
+	
 	
 }
